89179ebdb8432e9d62103bf7ee3d20462c3646e9
Iana Ionova <qa.specialist.pro@gmail.com>
2024-09-06 12:23:30 +0400

    Update test data

--- src/test/java/PracticeTextBoxTests.java
+++ src/test/java/PracticeTextBoxTests.java
@@ -19,38 +19,39 @@ public class PracticeTextBoxTests {
     @Test
     public void toolsQaForm () {
         open("/automation-practice-form");
+
+        // Удалить элементы рекламы
         Selenide.executeJavaScript("$('#fixedban').remove()");
         Selenide.executeJavaScript("$('footer').remove()");
 
-        $("#firstName").setValue("Oleg");
-        $("#lastName").setValue("Petrov");
-        $("#userEmail").setValue("PetrovOleg@mail.ru");
+        $("#firstName").setValue("Ivan");
+        $("#lastName").setValue("Ivanov");
+        $("#userEmail").setValue("IvanovIvan@mail.ru");
         $("#genterWrapper").$(byText("Male")).click();
-        $("#userNumber").setValue("9168130220");
+        $("#userNumber").setValue("9123456789");
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-dropdown-container").$(byText("March")).click();
-        $(".react-datepicker__year-dropdown-container").$(byText("1980")).click();
-        $(".react-datepicker__month-container").$(byText("13")).click();
+        $(".react-datepicker__month-dropdown-container").$(byText("July")).click();
+        $(".react-datepicker__year-dropdown-container").$(byText("1992")).click();
+        $(".react-datepicker__month-container").$(byText("25")).click();
         $("#subjectsInput").setValue("English").pressEnter();
         $("#hobbies-checkbox-3").parent().$(byText("Music")).click();
         $("#uploadPicture").uploadFromClasspath("pictures/1.png");
-        $("#currentAddress").setValue("Victory cq 5");
+        $("#currentAddress").setValue("Sunset Blvd 123");
         $("#state").click();
         $("#stateCity-wrapper").$(byText("Uttar Pradesh")).click();
         $("#city").click();
         $("#stateCity-wrapper").$(byText("Agra")).click();
         $("#submit").click();
         $(".modal-open").shouldHave(text("Thanks for submitting the form"));
-        $(".modal-body").shouldHave(text("Oleg Petrov"));
-        $(".modal-body").shouldHave(text("PetrovOleg@mail.ru"));
+        $(".modal-body").shouldHave(text("Ivan Ivanov"));
+        $(".modal-body").shouldHave(text("IvanovIvan@mail.ru"));
         $(".modal-body").shouldHave(text("Male"));
-        $(".modal-body").shouldHave(text("9168130220"));
-        $(".modal-body").shouldHave(text("13 March,1980"));
+        $(".modal-body").shouldHave(text("9123456789"));
+        $(".modal-body").shouldHave(text("25 July,1992"));
         $(".modal-body").shouldHave(text("English"));
         $(".modal-body").shouldHave(text("Music"));
-        $(".modal-body").shouldHave(text("Victory cq 5"));
+        $(".modal-body").shouldHave(text("Sunset Blvd 123"));
         $(".modal-body").shouldHave(text("Uttar Pradesh Agra"));
         $("#closeLargeModal").click();
-
     }
 }
